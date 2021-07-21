6f0a898614e91bfd4861439945c56a4be2e6287c
david vareba <davidvareba@gmail.com>
2021-07-20 21:32:26 -0500

    object-array created

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 console.log("CONNECTED!");
+let Team = [
+
+   { ID: 01,
+    name: "David",
+    favColor: "Red",
+    email: "myemail@email.com",
+    somethingUnique: "Seafood lover",
+    Image: "https://avatars.githubusercontent.com/u/86807856?v=4",
+    },
+
+
+   { ID: 02,
+    name: "Kristin",
+    favColor: "Blue",
+    email: "kamail@email.com",
+    somethingUnique: "leap baby",
+    Image: "https://avatars.githubusercontent.com/u/81543267?v=4",
+    },
+
+    {ID: 03,
+    name: "Harika",
+    favColor: "Red",
+    email: "hsuvak@email.com",
+    somethingUnique: "Knows 3 languages",
+    Image: "https://avatars.githubusercontent.com/u/79666889?v=4",
+    },
+
+    {ID: 04,
+    name: "Mary Beth",
+    favColor: "Green",
+    email: "mb@email.com",
+    somethingUnique: "brews kombucha",
+    Image: "https://avatars.githubusercontent.com/u/86667443?v=4",
+    }
+];
